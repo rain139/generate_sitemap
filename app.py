1c41e3e12d4f58223729da1ed0dbf20abe01e151
rain139 <rain139@ukr.net>
2019-07-07 11:27:58 +0300

    fix bugs

--- app.py
+++ app.py
@@ -5,7 +5,5 @@ from generator.GenerateSitemap import Sitemap
 home_page = "https://ksena.com.ua"
 
 links = ParserLink(home_page).run()
-print(links)
-import sys
-sys.exit(4)
+
 Sitemap(links, home_page).generate()
